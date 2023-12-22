18ea7c9b4039501744f04e13b9c25b7ad8cd9ef6
Deepanshu Kacher <kacher.deepanshu895@gmail.com>
2023-12-22 11:08:34 -0800

    production

--- useFullItems/constants/constants.ts
+++ useFullItems/constants/constants.ts
@@ -1,8 +1,8 @@
-const IS_PRODUCTION = false;
+const IS_PRODUCTION = true;
 
 export const constants = {
   IS_DEVELOPMENT: !IS_PRODUCTION,
   mqttSubsribeTopic: (restaurantId: string) => `${restaurantId}/tables/#`,
-  development_server_url: "http://192.168.105.48:5000/",
+  development_server_url: "http://192.168.105.14:5000/",
   production_server_url: "https://api.eatrofoods.com/",
 };
